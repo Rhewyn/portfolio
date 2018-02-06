5a5faf66aa1fd60d256b62ec237f2d330b6f6889
Richards <timothyrichards@Timothys-MacBook-Pro.local>
2018-02-06 11:55:09 -0700

    Switched 3001 for port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express()
-const port = process.env.PORT || 3001 
+const port = process.env.PORT || 3001
 app.use('/', express.static(path.join(__dirname, "public")));
 
-app.listen(3001, () => console.log("it works 3001"))
+app.listen(port, () => console.log("it works 3001"))
