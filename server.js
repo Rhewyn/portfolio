423ff28550dbc9f650bb0f95b66493df6294f8f7
Richards <timothyrichards@Timothys-MacBook-Pro.local>
2018-02-06 11:50:24 -0700

    Switch port order

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express()
-const port = 3001 || process.env.PORT
+const port = process.env.PORT || 3001 
 app.use('/', express.static(path.join(__dirname, "public")));
 
 app.listen(3001, () => console.log("it works 3001"))
